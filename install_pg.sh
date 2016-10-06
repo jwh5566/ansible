6a7d3df411db32cbfd6012ba0d0b3bb134192302
jwh5566 <jwh5566@foxmail.com>
2016-10-06 11:58:53 +0800

    inital

---
+++ install_pg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+tar xf postgresql-9.3.10.tar.gz
+cd postgresql-9.3.10
+./configure
+make
+make install
+adduser postgres
+mkdir /home/postgres/data
+chown postgres /home/postgres/data
+su - postgres
+/usr/local/pgsql/bin/initdb --encoding=utf8  -D /home/postgres/data
+/usr/local/pgsql/bin/postgres -D /home/postgres/data >logfile 2>&1 &
